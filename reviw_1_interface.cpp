3583a6a9e99e7bfcbf85c6f359304410c47aba04
sxVova <53950314+sxVova@users.noreply.github.com>
2019-10-15 07:51:14 +0300

    Update reviw_1_interface.cpp

--- reviw_1_interface.cpp
+++ reviw_1_interface.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include <algorithm>
 
 // The function returns an iterator that points 
 // to the first(maximally left) at least value 
@@ -13,13 +14,12 @@ std::vector<int>::iterator find_lower_bound(std::vector<int>::iterator start_of_
 int read_integer();
 
 // Read an information about the workers 
-// and reverse it for further calculations
-std::vector<int> read_reverse_integer_vector(const int vector_size);
+std::vector<int> read_integer_vector(const int vector_size);
 
 // Find the numbers of all employees from 
-// the longest growing subset
-std::vector<int> find_longest_growing_subset(
-    const std::vector<int> &data_input, const int vector_size);
+// the largest non - increasing sequence
+std::vector<int> find_indices_of_the_largest_non_increasing_sequence(
+    std::vector<int> data_input);
 
 // Write the size and content of the vector
 void write_vector_information(const std::vector<int> &informative_vector);
@@ -29,8 +29,9 @@ int main()
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
     const int vector_size = read_integer();
-    const std::vector<int> data_input = read_reverse_integer_vector(vector_size);
-    const std::vector<int> answer = find_longest_growing_subset(data_input, vector_size);
-    write_vector_information(answer);
+    const std::vector<int> employee_demand_ratios = read_integer_vector(vector_size);
+    const std::vector<int> indices_of_the_largest_non_increasing_sequence =
+        find_indices_of_the_largest_non_increasing_sequence(employee_demand_ratios);
+    write_vector_information(indices_of_the_largest_non_increasing_sequence);
     return 0;
 }
