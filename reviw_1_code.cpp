7f5f9810ae99af0b12290da084c386018567bb2f
sxVova <53950314+sxVova@users.noreply.github.com>
2019-10-23 19:02:29 +0300

    Update reviw_1_code.cpp

--- reviw_1_code.cpp
+++ reviw_1_code.cpp
@@ -10,13 +10,12 @@
 std::vector<int>::iterator find_lower_bound(const std::vector<int>::iterator& start_of_search,
     const std::vector<int>::iterator& end_of_search, const int insertion_element)
 {
-    std::vector<int>::iterator middle_separator;
     std::vector<int>::iterator place_to_insert = start_of_search;
     std::iterator_traits<std::vector<int>::iterator>::difference_type cut_length, search_step;
     cut_length = distance(place_to_insert, end_of_search);
 
     while (cut_length > 0) {
-        middle_separator = place_to_insert;
+        std::vector<int>::iterator middle_separator = place_to_insert;
         search_step = cut_length / 2;
         advance(middle_separator, search_step);
         if (*middle_separator <= insertion_element)
@@ -33,20 +32,20 @@ std::vector<int>::iterator find_lower_bound(const std::vector<int>::iterator& st
 }
 
 // Read the size of the input data
-int read_integer()
+int read_integer(std::istream& input = std::cin)
 {
     int variable;
-    std::cin >> variable;
+    input >> variable;
     return variable;
 }
 
 // Read an information about the workers 
-std::vector<int> read_integer_vector(const int vector_size)
+std::vector<int> read_integer_vector(const int vector_size, std::istream& input = std::cin)
 {
     std::vector<int> data_input(vector_size);
     for (int i = 0; i < vector_size; ++i) 
     {
-        std::cin >> data_input[i];
+        input >> data_input[i];
     }
     return data_input;
 }
@@ -55,9 +54,11 @@ std::vector<int> read_integer_vector(const int vector_size)
 // the largest non - increasing sequence
 std::vector<int> find_optimal_subsequence(const std::vector<int>& employee_demand_ratios)
 {
-    std::vector<int> reversed_employee_ratios = employee_demand_ratios;
-    std::reverse(reversed_employee_ratios.begin(), reversed_employee_ratios.end());
-    const int vector_size = reversed_employee_ratios.size();
+    std::vector<int> reversed_ratios;
+    copy(employee_demand_ratios.begin(), employee_demand_ratios.end(), 
+        back_inserter(reversed_ratios));
+    std::reverse(reversed_ratios.begin(), reversed_ratios.end());
+    const int vector_size = reversed_ratios.size();
     std::vector<int> largest_number_sequence_ends(vector_size + 1, INT_MAX);
     largest_number_sequence_ends[0] = INT_MIN;
     std::vector<int> position(vector_size + 1, 0);
@@ -72,19 +73,15 @@ std::vector<int> find_optimal_subsequence(const std::vector<int>& employee_deman
         pointer_to_the_place_to_insert = find_lower_bound(
             largest_number_sequence_ends.begin(), 
             largest_number_sequence_ends.end(), 
-            reversed_employee_ratios[current]
+            reversed_ratios[current]
         );
 
         insertion_index += pointer_to_the_place_to_insert - largest_number_sequence_ends.begin();
         
-        if (
-            largest_number_sequence_ends[insertion_index - 1] <= 
-            reversed_employee_ratios[current] &&
-            reversed_employee_ratios[current] <= 
-            largest_number_sequence_ends[insertion_index]
-            ) // bid validation
+        if (largest_number_sequence_ends[insertion_index - 1] <= reversed_ratios[current] && // bid
+            reversed_ratios[current] <= largest_number_sequence_ends[insertion_index]) // validation
         {
-            largest_number_sequence_ends[insertion_index] = reversed_employee_ratios[current];
+            largest_number_sequence_ends[insertion_index] = reversed_ratios[current];
             position[insertion_index] = current;
             previous_element[current] = position[insertion_index - 1];
             
@@ -108,19 +105,20 @@ std::vector<int> find_optimal_subsequence(const std::vector<int>& employee_deman
 }
 
 // Write the size and content of the vector
-void write_vector_information(const std::vector<int>& integer_vector)
+void write_vector_information(const std::vector<int>& integer_vector, 
+    std::ostream& output = std::cout)
 {
-    std::cout << integer_vector.size() << '\n';
+    output << integer_vector.size() << '\n';
     for (auto current_element : integer_vector) 
     {
-        std::cout << current_element << '\n';
+        output << current_element << '\n';
     }
 }
 
 int main()
 {
     std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+    std::cin.tie(0);
     const int vector_size = read_integer();
     const std::vector<int> employee_demand_ratios = read_integer_vector(vector_size);
     const std::vector<int> optimal_subsequence = find_optimal_subsequence(employee_demand_ratios);
