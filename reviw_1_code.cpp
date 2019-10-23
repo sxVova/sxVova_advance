f871b4e042b1b1d39cabea29df99621f20cd774a
sxVova <53950314+sxVova@users.noreply.github.com>
2019-10-23 16:03:11 +0300

    Update reviw_1_code.cpp

--- reviw_1_code.cpp
+++ reviw_1_code.cpp
@@ -7,28 +7,29 @@
 // to the first(maximally left) at least value 
 // comparable to "insertion_element" between the "start_of_search" and 
 // "end_of_search" of the given vector
-std::vector<int>::iterator find_lower_bound(const std::vector<int>::iterator &start_of_search,
-    const std::vector<int>::iterator &end_of_search, const int insertion_element)
+std::vector<int>::iterator find_lower_bound(const std::vector<int>::iterator& start_of_search,
+    const std::vector<int>::iterator& end_of_search, const int insertion_element)
 {
-    std::vector<int>::iterator temporary;
-    std::vector<int>::iterator resault = start_of_search;
-    std::iterator_traits<std::vector<int>::iterator>::difference_type count, step;
-    count = distance(resault, end_of_search);
-    while (count > 0) {
-        temporary = resault;
-        step = count / 2;
-        advance(temporary, step);
-        if (*temporary <= insertion_element)
+    std::vector<int>::iterator middle_separator;
+    std::vector<int>::iterator place_to_insert = start_of_search;
+    std::iterator_traits<std::vector<int>::iterator>::difference_type cut_length, search_step;
+    cut_length = distance(place_to_insert, end_of_search);
+
+    while (cut_length > 0) {
+        middle_separator = place_to_insert;
+        search_step = cut_length / 2;
+        advance(middle_separator, search_step);
+        if (*middle_separator <= insertion_element)
         {
-            resault = ++temporary;
-            count -= step + 1;
+            place_to_insert = ++middle_separator;
+            cut_length -= search_step + 1;
         }
         else
         {
-            count = step;
+            cut_length = search_step;
         }
     }
-    return resault;
+    return place_to_insert;
 }
 
 // Read the size of the input data
@@ -43,7 +44,8 @@ int read_integer()
 std::vector<int> read_integer_vector(const int vector_size)
 {
     std::vector<int> data_input(vector_size);
-    for (int i(0); i < vector_size; ++i) {
+    for (int i = 0; i < vector_size; ++i) 
+    {
         std::cin >> data_input[i];
     }
     return data_input;
@@ -51,48 +53,66 @@ std::vector<int> read_integer_vector(const int vector_size)
 
 // Find the numbers of all employees from 
 // the largest non - increasing sequence
-std::vector<int> find_optimal_subsequence(const std::vector<int> &employee_demand_ratios)
+std::vector<int> find_optimal_subsequence(const std::vector<int>& employee_demand_ratios)
 {
     std::vector<int> reversed_employee_ratios = employee_demand_ratios;
     std::reverse(reversed_employee_ratios.begin(), reversed_employee_ratios.end());
     const int vector_size = reversed_employee_ratios.size();
-    std::vector<int> dynamic_data(vector_size + 1, INT_MAX);
-    dynamic_data[0] = INT_MIN;
+    std::vector<int> largest_number_sequence_ends(vector_size + 1, INT_MAX);
+    largest_number_sequence_ends[0] = INT_MIN;
     std::vector<int> position(vector_size + 1, 0);
     position[0] = -1;
     std::vector<int> previous_element(vector_size, 0);
     int length_maximum_subsequence = 0;
     std::vector<int>::iterator pointer_to_the_place_to_insert;
-    for (int current(0); current < vector_size; ++current) {
+
+    for (int current = 0; current < vector_size; ++current) 
+    {
         int insertion_index(0);
         pointer_to_the_place_to_insert = find_lower_bound(
-            dynamic_data.begin(), dynamic_data.end(), reversed_employee_ratios[current]);
-        insertion_index += pointer_to_the_place_to_insert - dynamic_data.begin();
-        if (dynamic_data[insertion_index - 1] <= 
-            reversed_employee_ratios[current] && reversed_employee_ratios[current]
-            <= dynamic_data[insertion_index]) {
-            dynamic_data[insertion_index] = reversed_employee_ratios[current];
+            largest_number_sequence_ends.begin(), 
+            largest_number_sequence_ends.end(), 
+            reversed_employee_ratios[current]
+        );
+
+        insertion_index += pointer_to_the_place_to_insert - largest_number_sequence_ends.begin();
+        
+        if (
+            largest_number_sequence_ends[insertion_index - 1] <= 
+            reversed_employee_ratios[current] &&
+            reversed_employee_ratios[current] <= 
+            largest_number_sequence_ends[insertion_index]
+            ) // bid validation
+        {
+            largest_number_sequence_ends[insertion_index] = reversed_employee_ratios[current];
             position[insertion_index] = current;
             previous_element[current] = position[insertion_index - 1];
-            if (insertion_index > length_maximum_subsequence) {
+            
+            if (insertion_index > length_maximum_subsequence) // checking for the maximum 
+            {                                                 // length of a subsequence
                 length_maximum_subsequence = insertion_index;
             }
         }
     }
+
     std::vector<int> largest_non_increasing_sequence;
     int current_position = position[length_maximum_subsequence];
-    while (current_position != -1) {    // answer recovery
+
+    while (current_position != -1) // answer recovery
+    {    
         largest_non_increasing_sequence.push_back(vector_size - current_position);
         current_position = previous_element[current_position];
     }
+
     return largest_non_increasing_sequence;
 }
 
 // Write the size and content of the vector
-void write_vector_information(const std::vector<int> &integer_vector)
+void write_vector_information(const std::vector<int>& integer_vector)
 {
     std::cout << integer_vector.size() << '\n';
-    for (auto current_element : integer_vector) {
+    for (auto current_element : integer_vector) 
+    {
         std::cout << current_element << '\n';
     }
 }
